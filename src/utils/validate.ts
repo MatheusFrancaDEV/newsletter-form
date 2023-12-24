8cace89c3e34b59e2f40aabc98b4d6300866fedd
Matheus França <115188878+MatheusFrancaDEV@users.noreply.github.com>
2023-12-24 19:43:15 -0300

    Criando o restante das validações

--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -11,5 +11,13 @@ export const validate = (data: User) =>{
         errors['name'] = 'O nome é obrigatório'
     }
 
+    if(!data.email  ){
+        errors['email'] = 'O email é obrigatório'
+    }
+
+    if(!data.checkbox){
+        errors['checkbox'] = 'Você precisa concordar com os termos'
+    }
+
     return errors
 }
