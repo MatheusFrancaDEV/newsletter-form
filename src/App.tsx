871d3ee7e8e6cc237f20bab1e28493a9ef8d18d8
Matheus França <115188878+MatheusFrancaDEV@users.noreply.github.com>
2023-12-24 18:56:00 -0300

    Estrutura do App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import Form from "./components/Form"
+
 function App() {
 
   return (
-    <>
+    <div>
       <h1>Inscreva-se</h1>
-    </>
+      <p>Assine a newsletter e se mantenha-se sempre informado</p>
+      <div>
+        <Form />
+      </div>
+      <p>Ao se inscrever, você passará a receber as melhores noticias em primeira mão!</p>
+    </div>
   )
 }
 
