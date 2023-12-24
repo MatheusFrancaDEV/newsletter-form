0cce520b2e5de1ce2ddea5af4914cd544913fa02
Matheus França <115188878+MatheusFrancaDEV@users.noreply.github.com>
2023-12-24 19:17:47 -0300

    Aplicando a estilização basica

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import Form from "./components/Form"
 function App() {
 
   return (
-    <div>
-      <h1>Inscreva-se</h1>
-      <p>Assine a newsletter e se mantenha-se sempre informado</p>
-      <div>
+    <div className="bg-slate-300 min-h-screen w-full flex flex-col items-center justify-center">
+      <h1 className="text-slate-950 font-bold text-[2rem]">Inscreva-se</h1>
+      <p className="text-slate-800">Assine a newsletter e se mantenha-se sempre informado</p>
+      <div className="w-96 mt-4 bg-slate-200 px-4 py-5 rounded-lg ">
         <Form />
       </div>
-      <p>Ao se inscrever, você passará a receber as melhores noticias em primeira mão!</p>
+      <p className="text-slate-400 text-xs w-96 mt-2 text-center">Ao se inscrever, você passará a receber as melhores noticias em primeira mão!</p>
     </div>
   )
 }
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,22 +1,18 @@
 function Form() {
     return(
-        <form>
-            <div>
-                <label htmlFor="name">Nome</label>
-                <input type="text" name="nome" id="name" placeholder="Digite seu nome" />
-            </div>
-            <div>
-                <label htmlFor="email">E-mail</label>
-                <input type="email" name="email" id="email" placeholder="Digite seu email" />
-            </div>
-            <div>
-                <a href="#">Leia os termos</a>
-                <div>    
-                    <input type="checkbox" name="checkbox" id="checkbox"  />
-                    <label htmlFor="chekbox">Concordo com os termos</label>
+        <form className="flex flex-col gap-3 ">
+            <label className="text-sm" htmlFor="name">Nome</label>
+            <input type="text" className="rounded-lg py-2 px-2 text-sm placeholder:text-sm placeholder:text-stone-400 outline-none border-solid border border-slate-400 focus:border-slate-600" name="nome" id="name" placeholder="Digite seu nome" />
+            <label className="text-sm" htmlFor="email">E-mail</label>
+            <input type="email" className="rounded-lg py-2 px-2 text-sm placeholder:text-sm placeholder:text-stone-400 outline-none border-solid border border-slate-400 focus:border-slate-600" name="email" id="email" placeholder="Digite seu email" />
+            <div className="flex flex-col">
+                <a href="#" className="text-xs underline mb-2">Leia os termos</a>
+                <div className="flex gap-2 items-center">    
+                    <input type="checkbox" name="checkbox" id="checkbox" className="cursor-pointer"  />
+                    <label htmlFor="checkbox" className="cursor-pointer">Concordo com os termos</label>
                 </div>
             </div>
-            <button>Inscreva-se</button>
+            <button className="bg-slate-900 hover:bg-slate-700 font-medium text-sm py-3 px-4 rounded-lg text-white transition ease-in-out">Inscreva-se</button>
         </form>
     )
 }
